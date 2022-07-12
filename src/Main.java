7f3694094fd7bf24ef443b3f71b7ad654cdd7e4a
klepsla <kleslava3@gmail.com>
2022-07-12 08:35:22 +0300

    Задание № 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var weight = 10;
+        System.out.println(weight);
+        byte box = -100;
+        System.out.println(box);
+        short box1 = 25367;
+        System.out.println(box1);
+        int box2 = 123456789;
+        System.out.println(box2);
+        long box3 = 1055L;
+        System.out.println(box3);
+        float box4 = 3.14f;
+        System.out.println(box4);
+        double box5 = 1.123456789;
+        System.out.println(box5);
+
     }
 }
